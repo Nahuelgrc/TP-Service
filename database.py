9a6992652fe830f9a87c32c7fdd82793f1a04a4e
damiannutz <rubioden22@hotmail.com>
2021-06-19 15:14:39 -0300

    No more errors due to database multi-thread transactions

--- database.py
+++ database.py
@@ -9,7 +9,10 @@ Session = sessionmaker()
 def get_db_session():
   return Session()
 
+def close_session():
+  Session.close_all()
+
 def setup_database(app):
-  engine = create_engine('sqlite:///shopdb.sqlite')
+  engine = create_engine('sqlite:///shopdb.sqlite',connect_args={'check_same_thread': False})
   Base.metadata.create_all(engine, checkfirst=True)
   Session.configure(bind=engine)
